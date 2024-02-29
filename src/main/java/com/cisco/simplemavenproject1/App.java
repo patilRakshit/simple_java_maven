db7008bc7ad3ed174195a8f7d9eed20c6e029e27
patilRakshit <rakshitpatil42@gmail.com>
2024-02-29 11:51:07 +0530

    first commit

---
+++ src/main/java/com/cisco/simplemavenproject1/App.java
@@ -0,0 +1,10 @@
+package com.cisco.simplemavenproject1;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the kingdom of jenkins!!" );
+        System.out.println( "Hello everyone from jenkins.. simple project" );
+    }
+}
